0a03cf037181ae4b35bf88ef03b9b02cd358f172
vladyslav.bukach <vladyslav.bukach@globallogic.com>
2018-11-05 16:31:11 +0200

    init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,43 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID,} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5bdc4116d91ba629ad2ed56a')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    
+    db.collection('Users').find({name: 'Vladyslav'}).toArray().then((docs) => {
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch todos', err);
+    });
+
+    client.close();
+});
+
+
+
+
+
+// // Destructuring
+// let user = {name: 'vlad', age: 32};
+// let {name, age} = user;
+// console.log(name, age);
