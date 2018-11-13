e3cbe6c88c60d69a61c3bf049e5fcbad6230a2ca
Vl Bu <chinazaro@gmail.com>
2018-11-13 17:51:23 +0200

    test2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,14 +1,14 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://longtrain:123qwe123@ds261253.mlab.com:61253/todoapp' || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true });
 
 module.exports = {mongoose};
 
 
 // || 'mongodb://localhost:27017/TodoApp'
 
-//process.env.MONGOURI
+//process.env.MONGODBURI
 
 // heroku config:set MONGOLAB_URI="mongodb://longtrain:123qwe123@ds261253.mlab.com:61253/todoapp"
 
